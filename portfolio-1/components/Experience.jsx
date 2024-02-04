c26b7e693901a305336e5ce8e3cdbb8cf5963872
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-02-04 16:48:06 +0530

    c

---
+++ portfolio-1/components/Experience.jsx
@@ -0,0 +1,129 @@
+function Experience() {
+    return (
+        <>
+
+<div className="experience text-center p-12" id="experience ">
+      <div className="text-pink-500 text-lg">Know My</div>
+      <div className="text-pink-900 text-5xl font-bold">Experience</div>
+      <div className="grid grid-cols-2 p-10 px-32 gap-5">
+        <div className="ring-1 p-10 bg-red-100 rounded-lg">
+          <div className="text-pink-900 text-3xl font-bold">
+              Front End Development
+
+          </div>
+          <div className="grid grid-cols-3 py-10">
+            <div className="flex py-5">
+              <img src="checkmark.png" className="h-10" />
+                <div className="flex flex-col">
+                    <div className="text-pink-900 text-2xl font-bold">HTML</div>
+                    <div className="text-pink-600 text-xl font-semibold">Experienced</div>
+
+                </div>
+            </div>
+            <div className="flex py-5">
+              <img src="checkmark.png" className="h-10" />
+                <div className="flex flex-col">
+                    <div className="text-pink-900 text-2xl font-bold">NextJS</div>
+                    <div className="text-pink-600 text-xl font-semibold">Intermediate</div>
+
+                </div>
+            </div>
+            <div className="flex py-5">
+              <img src="checkmark.png" className="h-10" />
+                <div className="flex flex-col">
+                    <div className="text-pink-900 text-2xl font-bold">WordPress</div>
+                    <div className="text-pink-600 text-xl font-semibold">Intermediate</div>
+
+                </div>
+            </div>
+            <div className="flex py-5">
+              <img src="checkmark.png" className="h-10" />
+                <div className="flex flex-col">
+                    <div className="text-pink-900 text-2xl font-bold">JavaScript</div>
+                    <div className="text-pink-600 text-xl font-semibold">Experienced</div>
+
+                </div>
+            </div>
+            <div className="flex py-5">
+              <img src="checkmark.png" className="h-10" />
+                <div className="flex flex-col">
+                    <div className="text-pink-900 text-2xl font-bold">TailWind</div>
+                    <div className="text-pink-600 text-xl font-semibold">Intermediate</div>
+
+                </div>
+            </div>
+            <div className="flex py-5">
+              <img src="checkmark.png" className="h-10" />
+                <div className="flex flex-col">
+                    <div className="text-pink-900 text-2xl font-bold">ReactJS</div>
+                    <div className="text-pink-600 text-xl font-semibold">Intermediate</div>
+
+                </div>
+            </div>
+          </div>
+
+        </div>
+        <div className="ring-1 p-10 bg-red-100 rounded-lg">
+          <div className="text-pink-900 text-3xl font-bold">
+              Backend Development
+              
+          </div>
+          <div className="grid grid-cols-3 py-10">
+            <div className="flex py-5">
+              <img src="checkmark.png" className="h-10" />
+                <div className="flex flex-col">
+                    <div className="text-pink-900 text-2xl font-bold">Python</div>
+                    <div className="text-pink-600 text-xl font-semibold">Experienced</div>
+
+                </div>
+            </div>
+            <div className="flex py-5">
+              <img src="checkmark.png" className="h-10" />
+                <div className="flex flex-col">
+                    <div className="text-pink-900 text-2xl font-bold">Django</div>
+                    <div className="text-pink-600 text-xl font-semibold">Intermediate</div>
+
+                </div>
+            </div>
+            <div className="flex py-5">
+              <img src="checkmark.png" className="h-10" />
+                <div className="flex flex-col">
+                    <div className="text-pink-900 text-2xl font-bold">MongoDB</div>
+                    <div className="text-pink-600 text-xl font-semibold">Intermediate</div>
+
+                </div>
+            </div>
+            <div className="flex py-5">
+              <img src="checkmark.png" className="h-10" />
+                <div className="flex flex-col">
+                    <div className="text-pink-900 text-2xl font-bold">NodeJS</div>
+                    <div className="text-pink-600 text-xl font-semibold">Experienced</div>
+
+                </div>
+            </div>
+            <div className="flex py-5">
+              <img src="checkmark.png" className="h-10" />
+                <div className="flex flex-col">
+                    <div className="text-pink-900 text-2xl font-bold">Flask</div>
+                    <div className="text-pink-600 text-xl font-semibold">Beginner</div>
+
+                </div>
+            </div>
+            <div className="flex py-5">
+              <img src="checkmark.png" className="h-10" />
+                <div className="flex flex-col">
+                    <div className="text-pink-900 text-2xl font-bold">ReactJS</div>
+                    <div className="text-pink-600 text-xl font-semibold">Intermediate</div>
+
+                </div>
+            </div>
+            </div>
+          
+        </div>
+      </div>
+    </div>
+        </>
+    )
+}
+
+export default Experience;
---
+++ portfolio-1/src/App.jsx
@@ -0,0 +1,54 @@
+
+import Header from "../components/Header"
+import Navbar from "../components/Navbar"
+import About from "../components/About"
+import Experience from "../components/Experience"
+import Projects from "../components/Projects"
+import ContactUs from "../components/ContactUs"
+function App() {
+
+  return (
+    <>
+    <div className="body bg-black text-gray-500">
+      {/* Navbar */}
+    <Navbar/>
+      
+    {/* Navbar */}
+
+
+    {/* Header */}
+      <Header/>
+
+    {/* Header */}
+
+
+    {/* About */}
+
+    <About/>
+    {/* About */}
+
+
+
+    {/* Experience */}
+    <Experience/>
+
+    {/* Experience */}
+
+
+    {/* Projects */}
+
+      <Projects/>
+    {/* projects */}
+
+
+
+    {/* contact us */}
+    <ContactUs/>
+    {/* contact us */}
+
+    </div>
+    </>
+  )
+}
+
+export default App
